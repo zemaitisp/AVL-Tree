342ed121250c6f7fa5af01db99df1174b84ada76
Peter Zemaitis <zemaitis@protonmail.com>
2020-03-06 01:58:09 +0000

    update

--- AVLTree.java
+++ AVLTree.java
@@ -10,14 +10,172 @@ public class AVLTree {
     this.root = root;
   }
 
-  private void balanceIter() {
+  public void insertIter(int toInsert) {
+      if(root == null) {
+        root = new node(toInsert, null, null);
+        return;
+      }
+      node curr = root;
+      ArrayList<Node> ancestory = new ArrayList<Node>();
+      //if the current node is not empty, broken go on
+      //if the current node is equal to the new value break because assumed no duplicates
+      while(curr != null && curr.getKey() != toInsert) {
+        ancestory.add(curr);
+        if(curr.getKey() > toInsert) {
+          //if the value to insert is less than the current node and the left child is empty insert
+          if(curr.getLeft() == null) {
+            curr.setLeft(new node(toInsert, null, null));
+            break;
+          }
+          //else get the left node to compare
+          curr = curr.getLeft();
+        }
+        else {
+          //if the value to insert is greater than the current node and the right child is empty insert
+          if(curr.getRight() == null) {
+            curr.setRight(new node(toInsert, null, null));
+            break;
+          }
+          //else get the right child to compare
+          curr = curr.getRight();
+        }
+      }
+    for(int i = ancestory.size()-1; i >= 0; i--) {
+      if(Math.abs(getBF(ancestory.get(i))) >= 2){
+        balanceIter(ancestory);
+        break;
+      }
+    }
+    }
+
+    public void removeIter(int toRemove) {
+    if(root == null) {
+      return;
+    }
+    if(root.getKey() == toRemove) {
+      root = null;
+    }
+    node curr = root;
+    ArrayList<Node> ancestory = new ArrayList<Node>();
+    while(curr.getLeft().getKey() != toRemove && curr.getRight().getKey() != toRemove) {
+      if(curr.getKey() > toRemove) {
+        curr = curr.getLeft();
+      }
+      else {
+        curr = curr.getRight();
+      }
+    }
+    node temp;
+    node remove;
+    int toReplace;
+    //could probably make this a private method to remove clutter, but java makes copies of the obj so it might make a mess 
+    if(curr.getLeft().getKey() == toRemove){
+      temp = curr.getLeft();
+      //if both children are null just remove the element
+      if(temp.getLeft() == null && temp.getRight() == null) {
+        curr.setLeft(null);
+      }
+      //if the left child is null make the removed element the right node with its children
+      else if(temp.getLeft() == null) {
+            curr.setLeft(temp.getRight());
+          }
+      //if the right child is null make the removed element the left node with its children
+            else if(temp.getRight() == null) {
+              curr.setLeft(temp.getLeft());
+            }
+              else {
+                remove = temp.getRight();
+                while(remove.getLeft() != null) {
+                  remove = remove.getLeft();
+                }
+                toReplace = remove.getLeft().getKey();
+                remove.setLeft(null);
+                curr.getLeft().setKey(toReplace);
+            }
+    }
+    else{
+      temp = curr.getRight();
+      //if both children are null just remove the element
+      if(temp.getLeft() == null && temp.getRight() == null) {
+        curr.setRight(null);
+      }
+      //if the left child is null make the removed element the right node with its children
+      else if(temp.getLeft() == null) {
+            curr.setRight(temp.getRight());
+          }
+      //if the right child is null make the removed element the left node with its children
+            else if(temp.getRight() == null) {
+              curr.setRight(temp.getLeft());
+            }
+              else {
+                remove = temp.getRight();
+                while(remove.getLeft() != null) {
+                  remove = remove.getLeft();
+                }
+                toReplace = remove.getLeft().getKey();
+                remove.setLeft(null);
+                curr.getRight().setKey(toReplace);
+            }
+    }
+  }
+  private void balanceIter(ArrayList<Node> l) {
+    if(l.size() == 0){return;}
+    Node curr = null;
+    int currBF = 0, prevBF = 0;
+    for(int i = l.size() - 1; i >= 0; i--) {
+      curr = l.get(i);
+      prevBF = currBF;
+      currBF = getBF(curr);
+      //incase of floating point errors
+      if(Math.abs(currBF) >= 2 - .001) {
+        break;
+      }
+    }
+    if(currBF == -2) {
+      if(prevBF == -1) {
+        rotLeft(curr);
+      }
+      else {
+        rotRight(curr.getRight());
+        rotLeft(curr.getLeft());
+      }
+    }
+    else {
+      if(prevBF == 1) {
+        rotRight(curr);
+      }
+      else {
+        rotLeft(curr.getLeft());
+        rotRight(curr);
+      }
+    }
 
   }
 
-  private void setHeightsRec() {
-    if(root != null){
-      setHeightsRec(root);
+  private void setHeightsIter(ArrayList<Node> l) {
+    Node curr;
+    boolean isLeft;
+   for(int i = l.size() -2; i >= 0; i--) {
+    curr = l.get(i);
+    curr.setHeight(l.get(i+1).getHeight()+1);
+    if(curr.getRight() == l.get(i+1)) {
+      isLeft = false;
     }
+    else {
+      isLeft = true;
+    }
+
+    if(isLeft) {
+      if(curr.getHeight() < curr.getLeft().getHeight()-1) {
+        curr.setHeight(curr.getLeft().getHeight()+1);
+      }
+    }
+    else {
+      if(curr.getHeight() < curr.getRight().getHeight() - 1) {
+        curr.setHeight(curr.getRight().getHeight() + 1);
+      }
+    }
+   }
   }
 
   //will set the nodes height according to the current tree
@@ -69,18 +227,6 @@ public class AVLTree {
     if(i == root) {
       root = j;
     }
-  }
-
-  private void setHeightsIter(){
-    ArrayList<Node> arr = new ArrayList<Node>();
-    Node curr = root;
-    if(root = null) {
-      return;
-    }
-   while(curr.getLeft() != null) {
-     
-   }
-    
   }
   
   //will calculate the BF for a given node
